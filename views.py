758c5d976519085930576c9481a2cab438b7fea8
Vitalii <witalic2000@gmail.com>
2022-12-30 19:17:30 +0200

    add readme

--- views.py
+++ views.py
@@ -7,3 +7,16 @@ while True:
 4 update
 5 delete
 6 exit:""")
+    if choice == '1':
+        note = create_note()
+        notes.append(note)
+    if choice == '2':
+        pass
+    if choice == '3':
+        pass
+    if choice == '4':
+        pass
+    if choice == '5':
+        pass
+    if choice == '6':
+        break
